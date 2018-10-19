a6d5b7d7375c3a22ab69735b7179b23e8f027443
Abhishek <abhiabcl@gmail.com>
2018-10-19 11:57:41 -0400

    Initial draft code

---
+++ src/main/java/com/flavor/avchatserver/AvChatServerApplication.java
@@ -0,0 +1,12 @@
+package com.flavor.avchatserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvChatServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvChatServerApplication.class, args);
+	}
+}
